7c37f42d43d3369bde04243abce92d107a3e23ce
panos1998 <petridisp@ece.auth.gr>
2021-12-13 21:52:06 +0200

    lesson 9 fulfillment

--- lesson9( Data task).py
+++ lesson9( Data task).py
@@ -1,14 +1,18 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from collections import Counter
 import random
+
+import lesson3
 from lesson5 import inverse_normal_cdf
 import math
 import matplotlib.pyplot as plt
-from lesson4 import correlation
-from lesson3 import Matrix, Vector, make_matrix
+from lesson4 import correlation, standard_deviation
+from lesson3 import Matrix, Vector, make_matrix, vector_mean, subtract, magnitude, scalar_multiply
+from lesson7_Gradient_Descent import gradient_step
 
 # Αυτή είναι η μέθοδος για να ομαδοποιούμε στοιχεία σε κάδους ανάλογα το μέγεθος  της τιμής τους
 # και ανάλογα  το εύρος  τιμών(πλατος)  κάθε κάδου bucket_size
+
 
 def bucketize(point: float, bucket_size: float) -> float:
     return bucket_size * math.floor(point / bucket_size)
@@ -192,3 +196,286 @@ with open('file.txt') as f:
         else:
             data.append(maybe_stock)
             print(maybe_stock)
+
+
+data = [
+    StockPrice(symbol='MSFT',
+               date=datetime.date(2018, 12, 24),
+               closing_price=106.03,
+               #.....
+               )]
+
+#max_aapl_price = max(stock_price.closing_price
+ #                    for stock_price in data
+  #                   if stock_price.symbol == 'AAPL')
+
+from collections import defaultdict
+
+
+#max_prices: Dict[str, float] = defaultdict(lambda: float('-inf'))
+#for sp in data:
+ #   symbol, closing_price = sp.symbol, sp.closing_price
+  #  if closing_price > max_prices[symbol]:
+   #     max_prices[symbol] = closing_price
+
+prices: Dict[str, List[StockPrice]] = defaultdict(list)
+for sp in data:
+    prices[sp.symbol].append(sp)
+
+prices = {symbol: sorted(symbol_prices)
+          for symbol, symbol_prices in prices.items()}
+
+
+def pct_change(yesterday: StockPrice, today: StockPrice) -> float:
+    return today.closing_price / yesterday.closing_price - 1
+
+
+class DailyChange(NamedTuple):
+    symbol: str
+    date: datetime.date
+    pct_change: float
+
+
+def day_over_day_changes(prices: List[StockPrice]) -> List[DailyChange]:
+    return [DailyChange(symbol=today.symbol,
+                        date=today.date,
+                        pct_change=pct_change(yesterday, today))
+            for yesterday, today in zip(prices[0:], prices[1:])]
+
+
+all_changes = [change
+               for symbol_prices in prices.values()
+               for change in day_over_day_changes(symbol_prices)]
+
+#max_change = max(all_changes, key=lambda change: change.pct_change)
+#min_change = min(all_changes, key=lambda change: change.pct_change)
+#changes_by_month: List[DailyChange] = {month: [] for month in range(1, 13)}
+#for change in all_changes:
+ #   changes_by_month[change.date.month].append(change)
+
+#avg_daily_change = {month: sum(change.pct_change for change in changes) / len(changes)
+                    #for month, changes in changes_by_month.items()}
+
+
+a_to_b = lesson3.distance([63, 150], [67, 160])
+a_to_c = lesson3.distance([63, 150], [70, 171])
+b_to_c = lesson3.distance([67, 160], [70, 171])
+a_to_b = lesson3.distance([160, 150], [170.2, 160])
+a_to_c = lesson3.distance([160, 150], [177.8, 171])
+b_to_c = lesson3.distance([170.2, 160], [177.8, 171])
+
+
+def scale(data: List[Vector]) -> Tuple[Vector, Vector]:
+    dim = len(data[0])
+    means = vector_mean(data)
+    stdevs = [standard_deviation([vector[i] for vector in data])
+              for i in range(dim)]
+    return means, stdevs
+
+
+vectors = [[-3, -1, 1], [-1, 0, 1], [1, 1, 1]]
+means, stdevs = scale(vectors)
+assert means == [-1, 0, 1]
+assert stdevs == [2, 1, 0]
+
+
+def rescale(data: List[Vector]) -> List[Vector]:
+    dim = len(data[0])
+    means, stdevs = scale(data)
+    rescaled = [v[:] for v in data]
+    for v in rescaled:
+        for i in range(dim):
+            if stdevs[i] > 0:
+                v[i] = (v[i] - means[i]) / stdevs[i]
+    return rescaled
+
+
+means, stdevs = scale(rescale(vectors))
+assert means == [0, 0, 1]
+assert stdevs == [1, 1, 0]
+
+import tqdm
+
+for i in tqdm.tqdm(range(100), colour='blue'):
+    _ = [random.random() for _ in range(1000000)]
+
+
+def primes_up_to(n: int) -> List[int]:
+    primes = [2]
+    with tqdm.trange(3, n) as t:
+        for i in t:
+            i_is_prime = not any( i % p == 0 for p in primes)
+            if i_is_prime:
+                primes.append(i)
+            t.set_description(f"{len(primes)} primes")
+    return primes
+
+
+my_primes = primes_up_to(100_000)
+
+
+def de_mean(data: List[Vector]) -> List[Vector]:
+    mean = vector_mean(data)
+    return [subtract(vector, mean) for vector in data]
+
+
+def direction(w: Vector) -> Vector:
+    mag = magnitude(w)
+    return [w_i / mag for w_i in w]
+
+
+def directional_variance(data: List[Vector], w: Vector) -> float:
+    w_dir = direction(w)
+    return sum(lesson3.dot(v, w_dir) ** 2 for v in data)
+
+
+def directional_variance_gradient(data: List[Vector], w: Vector) -> Vector:
+    w_dir = direction(w)
+    return [sum(2 * lesson3.dot(v, w_dir) * v[i] for v in data)
+            for i in range(len(w))]
+
+
+def first_principal_component(data: List[Vector],
+                              n: int = 100,
+                              step_size: float = 0.1) -> Vector:
+    guess = [1.0 for _ in data[0]]
+    with tqdm.trange(n) as t:
+        for _ in t:
+            dv = directional_variance(data, guess)
+            gradient = directional_variance_gradient(data, guess)
+            guess = gradient_step(guess, gradient, step_size)
+            t.set_description(f"dev: {dv:.3f}")
+    return direction(guess)
+pca_data = [
+[20.9666776351559,-13.1138080189357],
+[22.7719907680008,-19.8890894944696],
+[25.6687103160153,-11.9956004517219],
+[18.0019794950564,-18.1989191165133],
+[21.3967402102156,-10.8893126308196],
+[0.443696899177716,-19.7221132386308],
+[29.9198322142127,-14.0958668502427],
+[19.0805843080126,-13.7888747608312],
+[16.4685063521314,-11.2612927034291],
+[21.4597664701884,-12.4740034586705],
+[3.87655283720532,-17.575162461771],
+[34.5713920556787,-10.705185165378],
+[13.3732115747722,-16.7270274494424],
+[20.7281704141919,-8.81165591556553],
+[24.839851437942,-12.1240962157419],
+[20.3019544741252,-12.8725060780898],
+[21.9021426929599,-17.3225432396452],
+[23.2285885715486,-12.2676568419045],
+[28.5749111681851,-13.2616470619453],
+[29.2957424128701,-14.6299928678996],
+[15.2495527798625,-18.4649714274207],
+[26.5567257400476,-9.19794350561966],
+[30.1934232346361,-12.6272709845971],
+[36.8267446011057,-7.25409849336718],
+[32.157416823084,-10.4729534347553],
+[5.85964365291694,-22.6573731626132],
+[25.7426190674693,-14.8055803854566],
+[16.237602636139,-16.5920595763719],
+[14.7408608850568,-20.0537715298403],
+[6.85907008242544,-18.3965586884781],
+[26.5918329233128,-8.92664811750842],
+[-11.2216019958228,-27.0519081982856],
+[8.93593745011035,-20.8261235122575],
+[24.4481258671796,-18.0324012215159],
+[2.82048515404903,-22.4208457598703],
+[30.8803004755948,-11.455358009593],
+[15.4586738236098,-11.1242825084309],
+[28.5332537090494,-14.7898744423126],
+[40.4830293441052,-2.41946428697183],
+[15.7563759125684,-13.5771266003795],
+[19.3635588851727,-20.6224770470434],
+[13.4212840786467,-19.0238227375766],
+[7.77570680426702,-16.6385739839089],
+[21.4865983854408,-15.290799330002],
+[12.6392705930724,-23.6433305964301],
+[12.4746151388128,-17.9720169566614],
+[23.4572410437998,-14.602080545086],
+[13.6878189833565,-18.9687408182414],
+[15.4077465943441,-14.5352487124086],
+[20.3356581548895,-10.0883159703702],
+[20.7093833689359,-12.6939091236766],
+[11.1032293684441,-14.1383848928755],
+[17.5048321498308,-9.2338593361801],
+[16.3303688220188,-15.1054735529158],
+[26.6929062710726,-13.306030567991],
+[34.4985678099711,-9.86199941278607],
+[39.1374291499406,-10.5621430853401],
+[21.9088956482146,-9.95198845621849],
+[22.2367457578087,-17.2200123442707],
+[10.0032784145577,-19.3557700653426],
+[14.045833906665,-15.871937521131],
+[15.5640911917607,-18.3396956121887],
+[24.4771926581586,-14.8715313479137],
+[26.533415556629,-14.693883922494],
+[12.8722580202544,-21.2750596021509],
+[24.4768291376862,-15.9592080959207],
+[18.2230748567433,-14.6541444069985],
+[4.1902148367447,-20.6144032528762],
+[12.4332594022086,-16.6079789231489],
+[20.5483758651873,-18.8512560786321],
+[17.8180560451358,-12.5451990696752],
+[11.0071081078049,-20.3938092335862],
+[8.30560561422449,-22.9503944138682],
+[33.9857852657284,-4.8371294974382],
+[17.4376502239652,-14.5095976075022],
+[29.0379635148943,-14.8461553663227],
+[29.1344666599319,-7.70862921632672],
+[32.9730697624544,-15.5839178785654],
+[13.4211493998212,-20.150199857584],
+[11.380538260355,-12.8619410359766],
+[28.672631499186,-8.51866271785711],
+[16.4296061111902,-23.3326051279759],
+[25.7168371582585,-13.8899296143829],
+[13.3185154732595,-17.8959160024249],
+[3.60832478605376,-25.4023343597712],
+[39.5445949652652,-11.466377647931],
+[25.1693484426101,-12.2752652925707],
+[25.2884257196471,-7.06710309184533],
+[6.77665715793125,-22.3947299635571],
+[20.1844223778907,-16.0427471125407],
+[25.5506805272535,-9.33856532270204],
+[25.1495682602477,-7.17350567090738],
+[15.6978431006492,-17.5979197162642],
+[37.42780451491,-10.843637288504],
+[22.974620174842,-10.6171162611686],
+[34.6327117468934,-9.26182440487384],
+[34.7042513789061,-6.9630753351114],
+[15.6563953929008,-17.2196961218915],
+[25.2049825789225,-14.1592086208169]
+]
+
+print(first_principal_component(de_mean(pca_data)))
+
+
+def project(v: Vector, w: Vector) -> Vector:
+    projection_length = lesson3.dot(v, w)
+    return scalar_multiply(projection_length, w)
+
+
+def remove_projection_from_vector(v: Vector, w: Vector) -> Vector:
+    return subtract(v, project(v, w))
+
+
+def remove_projection(data: List[Vector], w: Vector) -> List[Vector]:
+    return [remove_projection_from_vector(v, w) for v in data]
+
+
+def pca(data: List[Vector], num_components: int) -> List[Vector]:
+    components: List[Vector] = []
+    for _ in range(num_components):
+        component = first_principal_component(data)
+        components.append(component)
+        data = remove_projection(data, component)
+    return components
+
+
+def transform_vector(v: Vector, components: List[Vector]) -> Vector:
+    return [lesson3.dot(v, w) for w in components]
+
+
+def transform(data: List[Vector], components: List[Vector]) -> List[Vector]:
+    return [transform_vector(v, components) for v in data]
