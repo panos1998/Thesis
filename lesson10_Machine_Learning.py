bae91777acf6a2434b7f5ec422930f70eef1de08
panos1998 <petridisp@ece.auth.gr>
2021-12-16 00:28:48 +0200

    10 lesson  completed

---
+++ lesson10_Machine_Learning.py
@@ -0,0 +1,68 @@
+import random
+from typing import TypeVar, List, Tuple
+X = TypeVar('X')
+
+
+def split_data(data: List[X], prob: float) -> Tuple[List[X], List[X]]:
+    data = data[:]
+    random.shuffle(data)
+    cut = int(len(data) * prob)
+    return data[:cut], data[cut:]
+
+
+data = [n for n in range(1000)]
+train, test = split_data(data, 0.75)
+assert len(train) == 750
+assert len(test) == 250
+assert sorted(train + test) == data
+Y = TypeVar('Y')
+
+
+def train_test_split(xs: List[X], ys: List[Y], test_pct: float) -> Tuple[List[X], List[X], List[Y],
+                                                                                           List[Y]]:
+    idxs = [i for i in range(len(xs))]
+    train_idxs, test_idxs = split_data(idxs, 1 - test_pct)
+    return ([xs[i] for i in train_idxs],
+            [xs[i] for i in test_idxs],
+            [ys[i] for i in train_idxs],
+            [ys[i] for i in test_idxs])
+
+
+xs = [x for x in range(1000)]
+ys = [2 * x for x in xs]
+x_train, x_test, y_train, y_test = train_test_split(xs, ys, 0.25)
+assert len(x_train) == 750
+assert len(x_test) == 250
+assert all(y == 2 * x for x, y in zip(x_train, y_train))
+assert all(y == 2 * x for x, y in zip(x_test, y_test))
+
+
+def accuracy(tp: int, fp: int, fn: int, tn: int) -> float:
+    correct = tp + tn
+    total = tp + fp + fn + tn
+    return correct / total
+
+
+assert accuracy(70, 4930, 13930, 981070) == 0.9814
+
+
+def precision(tp: int, fp: int, fn: int, tn: int) -> float:
+    return tp / (tp + fn)
+
+
+assert precision(70, 4930, 13930, 981070) == 0.014
+
+
+def recall(tp: int, fp: int, fn: int, tn: int) -> float:
+    return tp / (tp + fn)
+
+
+assert recall(70, 4930, 13930, 981070) == 0.005
+
+
+def f1_score(tp: int, fp: int, fn: int, tn: int) -> float:
+    p = precision(tp, fp, fn, tn)
+    r = recall(tp, fp, fn, tn)
+
+    return 2 * p * r / (p + r)
+
