c74071b42ec0ffdfc7993232449f21d354ba17be
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:10:47 +0000

    Adding numeric_owner as keyword arguement

--- lesson12_Bayesian.py
+++ lesson12_Bayesian.py
@@ -155,7 +155,7 @@ for filename in FILES:
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tf, OUTPUT_DIR)
